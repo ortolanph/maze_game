67326569baad3490a4a823f7cf04806e6cdc4add
Paulo Ortolan (ext.) <Paulo.Ortolan@ctw.bmwgroup.com>
2021-02-17 15:54:45 +0000

    Code integrated

--- ColorPallete.py
+++ ColorPallete.py
@@ -1,17 +1,18 @@
 """This is a temporary class"""
 
 BASE_PALLETE = {
-    "START": {
-        "BACKGROUND": (0, 255, 0),
-        "WALLS": (0, 128, 0)
-    },
-    "NORMAL": {
+    1: {
         "BACKGROUND": (0, 0, 0),
         "WALLS": (128, 128, 128)
     },
-    "END": {
+    2: {
+        "BACKGROUND": (0, 255, 0),
+        "WALLS": (0, 128, 0)
+    },
+    3: {
         "BACKGROUND": (255, 0, 0),
         "WALLS": (128, 0, 0)
     },
     "PLAYER": (255, 255, 0)
 }
+
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import sys
 
 import pygame
 
+import argparse
+
 from ColorPallete import BASE_PALLETE
 from GameRoom import GameRoom
 from Player import Player
+from maze_api.maze import Maze
 
 arguments = sys.argv[1:]
 PLAYER_STEP = 10
@@ -15,6 +18,26 @@ def main():
     """ Main Program """
 
     # Call this function so the Pygame library can initialize itself
+    parser = argparse.ArgumentParser(
+        prog="mazegame",
+        description="Creates a Maze Game session",
+        epilog="Be challenged!"
+    )
+    parser.add_argument(
+        "--width",
+        help="Width of the maze (max = 999)",
+        type=int,
+        default=5
+    )
+    parser.add_argument(
+        "--height",
+        help="Height of the maze (max = 999)",
+        type=int,
+        default=5
+    )
+    args = parser.parse_args()
+    arguments = vars(args)
+
     pygame.init()
 
     # Create an 800x600 sized screen
@@ -23,40 +46,17 @@ def main():
     # Set the title of the window
     pygame.display.set_caption('Maze')
 
-    maze = [
-        [
-            GameRoom([2, 3], "NORMAL"),
-            GameRoom([4], "START"),
-            GameRoom([2], "NORMAL"),
-            GameRoom([3, 4], "NORMAL")
-        ],
-        [
-            GameRoom([1, 2, 3], "NORMAL"),
-            GameRoom([2, 4], "NORMAL"),
-            GameRoom([2, 3, 4], "NORMAL"),
-            GameRoom([1, 3, 4], "NORMAL")
-        ],
-        [
-            GameRoom([1, 2, 3], "NORMAL"),
-            GameRoom([3, 4], "NORMAL"),
-            GameRoom([1], "NORMAL"),
-            GameRoom([1], "NORMAL")
-        ],
-        [
-            GameRoom([1], "NORMAL"),
-            GameRoom([1, 2], "NORMAL"),
-            GameRoom([2, 4], "NORMAL"),
-            GameRoom([4], "END")
-        ]
-    ]
+    maze = Maze(arguments["width"], arguments["height"])
+    maze.create_maze()
 
     player = Player()
     movingsprites = pygame.sprite.Group()
     movingsprites.add(player)
 
-    current_room_x = 1
-    current_room_y = 0
-    current_room = maze[int(current_room_y)][int(current_room_x)]
+    maze_room = maze.start_room()
+    current_room_x = maze_room.x
+    current_room_y = maze_room.y
+    game_room = GameRoom(maze_room.exits, maze_room.kind)
 
     clock = pygame.time.Clock()
 
@@ -87,29 +87,33 @@ def main():
                 if event.key == pygame.K_DOWN:
                     player.changespeed(0, -PLAYER_STEP)
 
-        player.move(current_room.room_walls())
+        player.move(game_room.room_walls())
 
         if player.rect.x < -64:
             current_room_x -= 1
             player.rect.x = 790
+            maze_room = maze.room_at(current_room_x, current_room_y)
 
         if player.rect.x > 801:
             current_room_x += 1
             player.rect.x = 0
+            maze_room = maze.room_at(current_room_x, current_room_y)
 
         if player.rect.y < -64:
             current_room_y -= 1
             player.rect.y = 790
+            maze_room = maze.room_at(current_room_x, current_room_y)
 
         if player.rect.y > 801:
             current_room_y += 1
             player.rect.y = 0
+            maze_room = maze.room_at(current_room_x, current_room_y)
 
-        current_room = maze[int(current_room_y)][int(current_room_x)]
-        screen.fill(BASE_PALLETE[current_room.kind]["BACKGROUND"])
+        screen.fill(BASE_PALLETE[game_room.kind]["BACKGROUND"])
 
         movingsprites.draw(screen)
-        current_room.room_walls().draw(screen)
+        game_room = GameRoom(maze_room.exits, maze_room.kind)
+        game_room.room_walls().draw(screen)
 
         pygame.display.flip()
 
