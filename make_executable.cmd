a5adb755f235d012c563ff02ae6a34c3186e645c
Paulo Ortolan (ext.) <Paulo.Ortolan@ctw.bmwgroup.com>
2021-03-10 11:15:33 +0000

    Creating the make executable for windows

--- make_executable.cmd
+++ make_executable.cmd
@@ -2,6 +2,6 @@ pip3 install -r requirements.txt
 pyinstaller --onefile --noconsole maze_game.py
 cd dist
 xcopy ..\assets
-Compress-Archive -Path . -DestinationPath maze_game.zip
+Compress-Archive -Path * -DestinationPath maze_game.zip
 cd ..
 copy dist/maze_game.zip .
